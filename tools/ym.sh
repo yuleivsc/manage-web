522301a885bea0913364c80e14bc539e127fbf8b
yulei <yulei@china-qizhi.com>
2018-01-22 16:53:12 +0800

    9t

--- tools/ym.sh
+++ tools/ym.sh
@@ -3,7 +3,7 @@
 YMANAGE_URL='http://manage.yulei.org/taskstatus/commit'
 FILE_URL='https://raw.githubusercontent.com/yuleivsc/manage-web/master/tools/ym.sh'
 FILE_VERSION='0.8.5'
-FILE_DATE='$Date:2018-01-22T16:25:43+08:00$'
+FILE_DATE='$Date:2018-01-22T16:53:12+08:00$'
 
 usage(){
     echo "Usage: $0 [options --] [shell [argments]"
@@ -128,10 +128,10 @@ if [ ! $upgradeshell = "" ];
 then
    cmdline=$upgradeshell
    retcode=$cmdline
-   upgradeshell 2>&1 $tempresult
+   upgradeshell > $tempresult 2>&1 $tempresult
    echo $? > $tempstatus
 else
-   (eval $@; echo $? > $tempstatus) 2>&1 > $tempresult
+   (eval $@; echo $? > $tempstatus) > $tempresult 2>&1
 fi
 
 cat $tempresult | while read line ;
